f245f2b6cc17ef1f060870f2b2c30d6919f9975c
thecat9801 <eliasglick1@outlook.com>
2024-12-07 13:40:21 -0500

    patch-0

--- src/main/java/net/thecat/firstmod/item/ModItems.java
+++ src/main/java/net/thecat/firstmod/item/ModItems.java
@@ -1,5 +1,13 @@
 package net.thecat.firstmod.item;
 
+import net.thecat.firstmod.FirstMod;
+import net.minecraft.world.item.Item;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+
 public class ModItems {
+    public static final DeferredRegister<Item> ITEMS =
+           DeferredRegister.create(ForgeRegistries.ITEMS, FirstMod.MOD_ID);
+
 
 }
